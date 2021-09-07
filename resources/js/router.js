9a512c5ef12081471c7a6e876da854dc87cf328f
Giovanni Bertolini <bosciu@icloud.com>
2021-09-07 18:19:56 +0200

    Fixed router

--- resources/js/router.js
+++ resources/js/router.js
@@ -4,15 +4,28 @@ import VueRouter from "vue-router";
 Vue.use(VueRouter);
 
 import Homepage from "./components/pages/homepage/Homepage.vue";
+import Store from "./components/pages/store/Store.vue";
+import Show from "./components/pages/show/Show.vue";
 
 const router = new VueRouter({
     mode: "history",
     linkExactActiveClass: "active",
     routes: [{
-        path: "/",
-        name: "home",
-        component: Homepage
-    }]
+            path: "/",
+            name: "home",
+            component: Homepage
+        },
+        {
+            path: "/store",
+            name: "store",
+            component: Store
+        },
+        {
+            path: "/:slug/show",
+            name: "store",
+            component: Show
+        }
+    ]
 });
 
 export default router;

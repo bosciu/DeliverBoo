215eac1fc983860f0ebb67e2cd1c2925316e58b8
Giovanni Bertolini <bosciu@icloud.com>
2021-09-08 10:30:09 +0200

    Fixed API RestaurantSHow

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -3,16 +3,36 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Dish;
 use App\Restaurant;
 use App\DishCategory;
 use App\RestaurantType;
+use Illuminate\Support\Arr;
+
 
 use Illuminate\Http\Request;
 
 class RestaurantController extends Controller
 {
     public function getRestaurant($slug){
-        $restaurant = Restaurant::where('slug', $slug)->firstOrFail();
+        $restaurant = Restaurant::where('slug', $slug)->with('address')->firstOrFail();
+        $dishes = Dish::where('restaurant_id', $restaurant->id)->get();
+
+        $categoriesId = [];
+        $categories = [];
+       
+        foreach($dishes as $dish) {
+            if(!in_array($dish->dish_category_id,$categoriesId)) {
+                $categoriesId[]=$dish->dish_category_id;
+                $categories[$dish->dishCategory->name]=$dish->dishCategory;
+            }
+        }
+
+        $restaurant['dishes'] = $dishes;
+        $restaurant['dish_categories'] = $categories;
+
+
+
         return response()->json($restaurant);
     }
 

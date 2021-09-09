e452a62ed893a23616f981d337bb7c6adce9bdf9
BaccanoB <beatrice.baccano@yahoo.it>
2021-09-09 17:58:05 +0200

    collegamenti router link

--- resources/js/components/pages/homepage/Homepage.vue
+++ resources/js/components/pages/homepage/Homepage.vue
@@ -1,21 +1,39 @@
 <template>
-    <div id="homepage">
-        <Header />
-        <Main />
-        <Footer />
+    <div>
+      <div id="homepage" v-show="!loading">
+            <Header />
+            <Main />
+            <Footer />
+        </div>
+        <Loading v-show="loading" />  
     </div>
+    
 </template>
 
 <script>
 import Header from "../../common/Header";
 import Footer from "../../common/Footer";
-import Main from "../../pages/homepage/Main"
+import Loading from "../../common/Loading";
+import Main from "../../pages/homepage/Main";
+
 export default {
     name: "Homepage",
     components: {
         Header,
         Footer,
-        Main
+        Main,
+        Loading
+    },
+    data: function() {
+        return {
+            loading:true
+        }
+    },
+    created:function() {
+
+        setTimeout(()=>{
+            this.loading = false;
+        },1000);
     }
 };
 </script>

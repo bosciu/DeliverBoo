96d528befeb789aedbe194243b9f925fed45c1d1
Giovanni Bertolini <bosciu@icloud.com>
2021-09-10 09:26:59 +0200

    Completamento link e gestione carrello

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="app">
         <!-- <Homepage /> -->
-        <router-view></router-view>
+        <router-view :key="$route.fullPath"></router-view>
     </div>
 </template>
 
